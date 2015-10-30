7f960523f27c9e1b7d008ed8e73861435fb96dc9
Theo Anderson <theoanderson3@gmail.com>
2015-10-30 14:37:16 -0700

    deploy

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -19,7 +19,7 @@ mongoose.connect(uristring, function(err, res) {
 // connect to the database
 // mongoose.connect('mongodb://localhost/Orders_Customers');
 // specify the path to all of the models
-var models_path = __dirname + '/..//models'
+var models_path = __dirname + '/../models'
 // read all of the files in the models_path and for each one check if it is a javascript file before requiring it
 fs.readdirSync(models_path).forEach(function(file) {
   if(file.indexOf('.js') > 0) {
