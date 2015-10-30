d80e331f042d84a06b44f2e3d4d8ddee2400d74d
Theo Anderson <theoanderson3@gmail.com>
2015-10-30 14:22:42 -0700

    hi

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path');
 var store = express();
 var bodyParser = require('body-parser');
-
+var port = Number(process.env.PORT || 8000);
 store.use(express.static(path.join(__dirname, './client')));
 store.use(bodyParser.json());
 
@@ -12,6 +12,6 @@ require('./server/config/orders.js')(store);
 require('./server/config/products.js')(store);
 require('./server/config/reviews.js')(store);
 
-store.listen(Number(process.env.PORT), function(){
+store.listen(port, function(){
 	console.log("Listening");
 });
